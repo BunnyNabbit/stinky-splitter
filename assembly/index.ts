8c8842ad80d03d75efc5551c1b3daf394ca76912
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-06-02 13:34:42 -0500

    Autosplit on level transition

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,10 +1,14 @@
 import "asr-assemblyscript/runtime"
 import { SmartI32Watcher } from "./SmartI32Watcher"
 import * as Timer from "asr-assemblyscript/timer"
+import * as UserSettings from "asr-assemblyscript/userSettings"
 import * as Process from "asr-assemblyscript/process"
 
+let initialized: bool = false
 let currentProcessId: Process.ProcessId = 0
 let resetting: bool = true
+let stagesEntered: Set<number> = new Set()
+	stagesEntered.add(1)
 
 const timeWatcher = new SmartI32Watcher("ggg.exe", 0x15d0a0)
 const stageWatcher = new SmartI32Watcher("ggg.exe", 0x15115c)
@@ -15,10 +19,20 @@ const showExpandedTimerWatcher = new SmartI32Watcher("ggg.exe", 0x169a10)
 const ofgm114Id: i32 = 11
 /** Goal */
 const ofgm122Id: i32 = 15
-/** First level's music */
-const ofgm113Id: i32 = 10
+
+let splitOnUniqueStage: bool = false
+
+function resetState(): void {
+	stagesEntered = new Set()
+	stagesEntered.add(1)
+	resetting = true
+}
 
 export function update(): void {
+	if (!initialized) {
+		splitOnUniqueStage = UserSettings.addBool("split_on_unique_stage", "Split on unique stage changes", true)
+		initialized = true
+	}
 	Timer.pauseGameTime()
 	if (currentProcessId === 0) {
 		currentProcessId = Process.attach("ggg.exe")
@@ -36,22 +50,22 @@ export function update(): void {
 	deathsWatcher.update(currentProcessId)
 	playingMusicWatcher.update(currentProcessId)
 	showExpandedTimerWatcher.update(currentProcessId)
+	if (splitOnUniqueStage && !resetting && stageWatcher.changed && !stagesEntered.has(stageWatcher.current)) {
+		stagesEntered.add(stageWatcher.current)
+		Timer.split()
+	}
 	const timeTicks = timeWatcher.current
 	const displayNanoseconds: f64 = (timeTicks % 60) * 16666666.666666666
 	const displaySeconds: i64 = timeTicks / 60
 	Timer.setGameTime(displaySeconds, displayNanoseconds as i32)
 	if (playingMusicWatcher.changed) {
 		if (playingMusicWatcher.current === ofgm114Id) {
-			Timer.reset()
-			resetting = true
+			resetState()
 		} else if (playingMusicWatcher.current === ofgm122Id) {
 			Timer.split()
-		} else if (resetting && playingMusicWatcher.current === ofgm113Id) {
-			resetting = false
-			Timer.start()
 		}
 	}
-	if (resetting && showExpandedTimerWatcher.changed) {
+	if (resetting && showExpandedTimerWatcher.changed && showExpandedTimerWatcher.current === 0) {
 		resetting = false
 		Timer.start()
 	}
